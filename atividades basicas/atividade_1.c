5d4c2dbb38a5376e52159e0b7ca530b7befff669
Daniel Costa <dan.costa.andrade@gmail.com>
2025-01-14 20:16:58 -0300

    Update atividade_1.c

--- atividades basicas/atividade_1.c
+++ atividades basicas/atividade_1.c
@@ -1,23 +1,23 @@
+// Essa atividade consiste em identificar animais que foram selecionados pelo criador da atividade a partir de certas classificações biológicas, como alimentação e classe.
+
 #include <stdio.h>
 #include <string.h>
 
 int main() {
-    char ossos[15], familia[15], alimentacao[15];
+    char ossos[15], classe[15], alimentacao[15];
     
-    // Leitura das entradas
     scanf("%s", ossos);
-    scanf("%s", familia);
+    scanf("%s", classe);
     scanf("%s", alimentacao);
     
-    // Comparação das strings
     if (strcmp(ossos, "vertebrado") == 0) {
-        if (strcmp(familia, "ave") == 0) {
+        if (strcmp(classe, "ave") == 0) {
             if (strcmp(alimentacao, "carnivoro") == 0) {
                 printf("aguia\n");
             } else if (strcmp(alimentacao, "onivoro") == 0) {
                 printf("pomba\n");
             }
-        } else if (strcmp(familia, "mamifero") == 0) {
+        } else if (strcmp(classe, "mamifero") == 0) {
             if (strcmp(alimentacao, "onivoro") == 0) {
                 printf("homem\n");
             } else if (strcmp(alimentacao, "herbivoro") == 0) {
@@ -25,13 +25,13 @@ int main() {
             }
         }
     } else if (strcmp(ossos, "invertebrado") == 0) {
-        if (strcmp(familia, "inseto") == 0) {
+        if (strcmp(classe, "inseto") == 0) {
             if (strcmp(alimentacao, "hematofago") == 0) {
                 printf("pulga\n");
             } else if (strcmp(alimentacao, "herbivoro") == 0) {
                 printf("lagarta\n");
             }
-        } else if (strcmp(familia, "anelideo") == 0) {
+        } else if (strcmp(classe, "anelideo") == 0) {
             if (strcmp(alimentacao, "hematofago") == 0) {
                 printf("sanguessuga\n");
             } else if (strcmp(alimentacao, "onivoro") == 0) {
